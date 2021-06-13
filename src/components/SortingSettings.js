78250df1037ccd86882b4078c50a0e9f58f3cfc6
murtazaaylak <murtazaaylak@gmail.com>
2021-06-13 10:33:03 +0300

    sorting component design

--- src/components/SortingSettings.js
+++ src/components/SortingSettings.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import { Button, ButtonGroup } from 'react-bootstrap'
 
 const SortingSettings = () => {
-  return <div className='justify-content-center'>Sorting Settings</div>
+  return (
+    <div className='demo my-3'>
+      <ButtonGroup>
+        <Button type='button' variant='light' className='sorting-button'>
+          Rating
+        </Button>
+        <Button type='button' variant='light' className='sorting-button'>
+          Asc
+        </Button>
+        <Button type='button' variant='light' className='sorting-button'>
+          Desc
+        </Button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button type='button' variant='light' className='sorting-button'>
+          <div className>
+            <i className='far fa-square'></i> <i className='far fa-square'></i>
+          </div>
+        </Button>
+        <Button type='button' variant='light' className='sorting-button'>
+          <div className>
+            <i className='far fa-square'></i> <i className='far fa-square'></i>{' '}
+            <i className='far fa-square'></i>
+          </div>
+        </Button>
+        <Button type='button' variant='light' className='sorting-button'>
+          <div className>
+            <i className='far fa-square'></i> <i className='far fa-square'></i>{' '}
+            <i className='far fa-square'></i> <i className='far fa-square'></i>
+          </div>
+        </Button>
+      </ButtonGroup>
+    </div>
+  )
 }
 
 export default SortingSettings
