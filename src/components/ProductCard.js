7991697c9b5bd849d6b42a1f55b9fba3d30ac3ea
murtazaaylak <murtazaaylak@gmail.com>
2021-06-13 22:20:23 +0300

    carousel indicator and dynamic images

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Carousel } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const ProductCard = ({ item }) => {
   return (
-    <Card className='h-100 product-card text-center'>
-      <Link to={`/product/${item.id}`} className='product-link'>
-        <Card.Img variant='top' src={item.image[0]} className='p-3' />
-        <Card.Body className='product-card-body'>
-          <Card.Title className='text-danger'>{item.name}</Card.Title>
-          <Card.Text className='text-dark'>$ {item.price}</Card.Text>
-        </Card.Body>
-      </Link>
+    <Card className='h-100 product-card text-center demo1'>
+      {/* <Link to={`/product/${item.id}`} className='product-link'> */}
+      <Carousel variant='light' interval={null} slide={false} indicator={true}>
+        {/* map image array to create carousel images */}
+        {item.image.map((i) => (
+          <Carousel.Item>
+            <Card.Img variant='top' src={i} className='p-3 d-block w-100' />
+            <Carousel.Caption />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <Card.Body className='product-card-body'>
+        <Card.Title className='text-danger'>{item.name}</Card.Title>
+        <Card.Text className='text-dark'>$ {item.price}</Card.Text>
+      </Card.Body>
+      {/* </Link> */}
     </Card>
   )
 }
