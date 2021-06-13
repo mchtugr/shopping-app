82eaa577d804051e14b492f4f33dd4e050dd4765
murtazaaylak <murtazaaylak@gmail.com>
2021-06-13 22:59:31 +0300

    Rating component created and added to ProductCard component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import { Card, Carousel } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Rating from './Rating'
 
 const ProductCard = ({ item }) => {
   return (
     <Card className='h-100 product-card text-center demo1'>
-      {/* <Link to={`/product/${item.id}`} className='product-link'> */}
-      <Carousel variant='light' interval={null} slide={false} indicator={true}>
+      <Carousel variant='light' interval={null} slide={false}>
         {/* map image array to create carousel images */}
-        {item.image.map((i) => (
-          <Carousel.Item>
-            <Card.Img variant='top' src={i} className='p-3 d-block w-100' />
+        {item.image.map((img, index) => (
+          <Carousel.Item key={index}>
+            <Link to={`/product/${item.id}`} className='product-link'>
+              <Card.Img variant='top' src={img} className='p-3 d-block w-100' />
+            </Link>
             <Carousel.Caption />
           </Carousel.Item>
         ))}
       </Carousel>
-      <Card.Body className='product-card-body'>
-        <Card.Title className='text-danger'>{item.name}</Card.Title>
-        <Card.Text className='text-dark'>$ {item.price}</Card.Text>
-      </Card.Body>
-      {/* </Link> */}
+      <Link to={`/product/${item.id}`} className='product-link'>
+        <Card.Body className='product-card-body'>
+          <Rating value={item.rating} />
+          <Card.Title className='text-danger'>{item.name}</Card.Title>
+          <Card.Text className='text-dark'>$ {item.price}</Card.Text>
+        </Card.Body>
+      </Link>
     </Card>
   )
 }
