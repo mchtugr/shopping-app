4d41f4c8ff1b033243cada90f1ce94b4f7ad5eac
murtazaaylak <murtazaaylak@gmail.com>
2021-06-13 23:41:34 +0300

    favorite icon added

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import { Card, Carousel } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Rating from './Rating'
 
 const ProductCard = ({ item }) => {
+  const [isFavorite, setIsFavorite] = useState(false)
+
+  const handleClick = () => {
+    setIsFavorite(!isFavorite)
+  }
   return (
     <Card className='h-100 product-card text-center demo1'>
+      <span onClick={handleClick} className='favorite-icon'>
+        <i
+          className={isFavorite ? 'fas fa-heart' : 'far fa-heart'}
+          style={{ fontSize: '20px' }}
+        ></i>
+      </span>
       <Carousel variant='light' interval={null} slide={false}>
         {/* map image array to create carousel images */}
         {item.image.map((img, index) => (
@@ -20,8 +32,8 @@ const ProductCard = ({ item }) => {
       <Link to={`/product/${item.id}`} className='product-link'>
         <Card.Body className='product-card-body'>
           <Rating value={item.rating} />
-          <Card.Title className='text-danger'>{item.name}</Card.Title>
-          <Card.Text className='text-dark'>$ {item.price}</Card.Text>
+          <Card.Text>{item.name}</Card.Text>
+          <Card.Title>$ {item.price}</Card.Title>
         </Card.Body>
       </Link>
     </Card>
